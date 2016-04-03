e6a914a24832edf97bb4ce0374a48cecf0573927
morozovivan95 <morozov.ivan95@yandex.ru>
2016-04-03 17:18:54 +0300

    JSONDecoder add two methods

--- src/jsonbeans/JSONDecoder.java
+++ src/jsonbeans/JSONDecoder.java
@@ -4,4 +4,17 @@ package jsonbeans;
  * Created by Morozov Ivan on 19.03.2016.
  */
 public class JSONDecoder {
+
+    private Object readObject(String source){
+
+
+
+
+
+        return null;
+    }
+
+    public Object readJSON(String jsonString){
+        return readObject(jsonString);
+    }
 }
---
+++ src/jsonbeans/JSONDeserializationException.java
@@ -0,0 +1,7 @@
+package jsonbeans;
+
+/**
+ * Created by Morozov Ivan on 03.04.2016.
+ */
+public class JSONDeserializationException extends Exception {
+}
