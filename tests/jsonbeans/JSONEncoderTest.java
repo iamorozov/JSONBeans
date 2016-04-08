c89ab4d010fb5172a030fbc4471a273e616b637e
morozovivan95 <morozov.ivan95@yandex.ru>
2016-04-08 17:11:27 +0300

    Unit test improve

--- tests/jsonbeans/JSONEncoderTest.java
+++ tests/jsonbeans/JSONEncoderTest.java
@@ -7,41 +7,11 @@ import org.junit.Assert.*;
 
 public class JSONEncoderTest {
 
-    /*Test fields*/
-    Dolphin dolphin1 = new StripedDolphin();
-    Dolphin dolphin2 = new DottetDolphin();
-    Dolphin dolphin3 = new Dolphin();
-
-    Trainer trainer1 = new Trainer();
-    Trainer trainer2 = new Trainer();
-
-    Dolphinarium dolphinarium = new Dolphinarium();
-
-    void buildObjectGraph(){
-
-        dolphin1.setName("Amicus");
-        dolphin2.setName("Fidget");
-        dolphin3.setName("Flipper");
-
-        dolphin1.setDolphinarium(dolphinarium);
-        dolphin2.setDolphinarium(dolphinarium);
-        dolphin3.setDolphinarium(dolphinarium);
-
-        trainer1.setDolphins(new Dolphin[]{dolphin1, dolphin2, dolphin3});
-        trainer2.setDolphins(new Dolphin[]{dolphin1, dolphin2});
-
-        trainer1.setName("John");
-        trainer2.setName("Sarah");
-
-        dolphinarium.setFirstTrainer(trainer1);
-        dolphinarium.setSecondTrainer(trainer2);
-    }
-
-
     @Test
     public void jsonEncoderTest(){
 
-        buildObjectGraph();
+        Dolphinarium dolphinarium = DolphinsGraph.buildDolphinarium();
+
         JSONEncoder jsonEncoder = new JSONEncoder();
 
         Assert.assertNotNull(dolphinarium);
--- tests/jsonbeans/XMLEncoderTest.java
+++ tests/jsonbeans/XMLEncoderTest.java
@@ -10,42 +10,12 @@ import java.io.OutputStream;
 
 public class XMLEncoderTest {
 
-    /*Test fields*/
-    Dolphin dolphin1 = new StripedDolphin();
-    Dolphin dolphin2 = new DottetDolphin();
-    Dolphin dolphin3 = new Dolphin();
-
-    Trainer trainer1 = new Trainer();
-    Trainer trainer2 = new Trainer();
-
-    Dolphinarium dolphinarium = new Dolphinarium();
-
-    void buildObjectGraph(){
-
-        dolphin1.setName("Amicus");
-        dolphin2.setName("Fidget");
-        dolphin3.setName("Flipper");
-
-        dolphin1.setDolphinarium(dolphinarium);
-        dolphin2.setDolphinarium(dolphinarium);
-        dolphin3.setDolphinarium(dolphinarium);
-
-        trainer1.setDolphins(new Dolphin[]{dolphin1, dolphin2, dolphin3});
-        trainer2.setDolphins(new Dolphin[]{dolphin1, dolphin2});
-
-        trainer1.setName("John");
-        trainer2.setName("Sarah");
-
-        dolphinarium.setFirstTrainer(trainer1);
-        dolphinarium.setSecondTrainer(trainer2);
-    }
-
 
     @Test
     public void xmlEncoderTest() {
         /*java.Beans XMLEncoder*/
 
-        buildObjectGraph();
+        Dolphinarium dolphinarium = DolphinsGraph.buildDolphinarium();
 
         try {
             OutputStream outputStream = new FileOutputStream("out.xml");
