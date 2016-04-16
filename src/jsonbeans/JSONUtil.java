30d1f341951cd459dc29dfab8f1b13e5826f5545
morozovivan95 <morozov.ivan95@yandex.ru>
2016-04-16 14:31:23 +0300

    JSONUtil class changed

--- src/jsonbeans/JSONUtil.java
+++ src/jsonbeans/JSONUtil.java
@@ -9,15 +9,37 @@ import java.util.Set;
  *
  * Contains some utility information
  */
-public class JSONUtil {
-    public static Set<Class> primitiveSet = new HashSet<>(
-            Arrays.asList(new Class[] {
+class JSONUtil {
+    static Set<Class> primitiveSet = new HashSet<>(
+            Arrays.asList(new Class[]{
                     Integer.class, Double.class, Float.class,
                     Boolean.class, String.class, Class.class,
                     Byte.class, Short.class, Long.class,
                     Character.class, int.class, double.class,
                     float.class, boolean.class, byte.class,
                     short.class, long.class, char.class
+            })
+    );
+
+    static Set<Class> numberTypes = new HashSet<>(
+            Arrays.asList(new Class[]{
+                    Integer.class, Byte.class, Short.class,
+                    Double.class, Float.class, Long.class,
+                    int.class, byte.class,
+                    short.class, long.class,
+                    double.class, float.class
+            })
+    );
+
+    static Set<Class> logicalTypes = new HashSet<>(
+            Arrays.asList(new Class[]{
+                    Boolean.class, boolean.class
+            })
+    );
+
+    static Set<Class> characterSequenceTypes = new HashSet<>(
+            Arrays.asList(new Class[]{
+                    String.class, Character.class, char.class
             })
     );
 }
