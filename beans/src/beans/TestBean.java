267c09474fe8ce7596dff2257a9b58501d4f1b59
morozovivan95 <morozov.ivan95@yandex.ru>
2016-04-16 14:29:39 +0300

    Some test classes change TestBean class created

---
+++ beans/src/beans/TestBean.java
@@ -0,0 +1,91 @@
+package beans;
+
+
+public class TestBean {
+
+    //Numerical primitives
+    private byte b;
+    private short sh;
+    private int i;
+    private long l;
+    private float f;
+    private double d;
+
+    //Other primitives
+
+    private String str;
+    private char ch;
+    private boolean bool;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public char getCh() {
+        return ch;
+    }
+
+    public void setCh(char ch) {
+        this.ch = ch;
+    }
+
+    public boolean isBool() {
+        return bool;
+    }
+
+    public void setBool(boolean bool) {
+        this.bool = bool;
+    }
+
+    public byte getB() {
+        return b;
+    }
+
+    public void setB(byte b) {
+        this.b = b;
+    }
+
+    public short getSh() {
+        return sh;
+    }
+
+    public void setSh(short sh) {
+        this.sh = sh;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public long getL() {
+        return l;
+    }
+
+    public void setL(long l) {
+        this.l = l;
+    }
+
+    public float getF() {
+        return f;
+    }
+
+    public void setF(float f) {
+        this.f = f;
+    }
+
+    public double getD() {
+        return d;
+    }
+
+    public void setD(double d) {
+        this.d = d;
+    }
+}
