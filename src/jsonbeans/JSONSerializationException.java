93e4db90ae4270f46d8675429b960df1e48ab1f1
morozovivan95 <morozov.ivan95@yandex.ru>
2016-05-04 22:19:09 +0300

    Fix

--- src/jsonbeans/JSONSerializationException.java
+++ src/jsonbeans/JSONSerializationException.java
@@ -2,6 +2,8 @@ package jsonbeans;
 
 /**
  * Created by Morozov Ivan on 07.03.2016.
+ *
+ * Class for JSON serialization exceptions
  */
 public class JSONSerializationException extends Exception {
 
